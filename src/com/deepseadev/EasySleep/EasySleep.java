343bc37861ea00aea4473c0a4c45748e2e51b680
Harry <harrybest@live.co.uk>
2016-08-09 17:50:04 +0100

    Added the base code

--- src/com/deepseadev/EasySleep/EasySleep.java
+++ src/com/deepseadev/EasySleep/EasySleep.java
@@ -1,5 +1,21 @@
 package com.deepseadev.EasySleep;
 
-public class EasySleep {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasySleep extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+	
+	//This is where the methods will go
 
 }
---
+++ src/com/deepseadev/EasySleep/SleepListener.java
@@ -0,0 +1,8 @@
+package com.deepseadev.EasySleep;
+
+import org.bukkit.event.Listener;
+
+// This is where the events are at.
+public class SleepListener implements Listener{
+	
+}
